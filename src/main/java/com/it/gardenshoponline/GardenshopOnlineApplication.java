c0d715dacd801c31ade4fe39cdcf2c723d391d30
T. Banharak <623170010119@rmu.ac.th>
2022-06-22 01:58:06 +0700

    finalproject

---
+++ src/main/java/com/it/gardenshoponline/GardenshopOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.it.gardenshoponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenshopOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenshopOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/it/gardenshoponline/GardenshopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.gardenshoponline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenshopOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
